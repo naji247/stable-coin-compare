932050294a86207feb26613631155f3aa938bd1f
naji247 <naji247@gmail.com>
2018-10-02 22:01:09 -0700

    Added coin selectorrs

--- src/routes/coins/Coins.js
+++ src/routes/coins/Coins.js
@@ -15,12 +15,44 @@ import Shake from 'react-reveal/Shake';
 import { connect } from 'react-redux';
 import { Element } from 'react-scroll';
 import { APP_URL } from '../../secrets';
-import Navbar from "../../components/Navbar";
+import Navbar from '../../components/Navbar';
+import _ from 'lodash';
+import * as coinDetails from '../../stablecoinInfo'
+
+function importAll(r) {
+  const images = {};
+  r.keys().map((item, index) => {
+    images[item.replace('./', '')] = r(item);
+  });
+  return images;
+}
+
+const coinLogos = importAll(
+  require.context('./icons', false, /\.(png|jpe?g|svg)$/)
+);
 
 class Coins extends React.Component {
   render() {
     return (
-      <Navbar />
+      <Element>
+        <Navbar />
+        <div className={s.container}>
+          <h1>Coins</h1>
+          <div className={s.coinDetailsContainer}>
+            <div className={s.coinSelectContainer}>
+              {_.keys(coinDetails).map(coinId =>
+                <div className={s.coinSelect}>
+                  <img key={coinId} src={coinLogos[`${coinId}.png`]} className={s.coinLogo} alt={coinId} />
+                  <span className={s.coinName}>{coinDetails[coinId]["Stablecoin Project"]}</span>
+                </div>
+              )}
+            </div>
+            <div className={s.coinDetails}>
+              <p>Select a coin from the left.</p>
+            </div>
+          </div>
+        </div>
+      </Element>
     );
   }
 }
